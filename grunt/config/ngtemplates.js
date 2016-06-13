9b23c51931e07e6db511830af02d05ebc1336569
Tiety Kooistra <tietyk@hotmail.com>
2016-06-13 11:53:37 +0200

    wiredep pakt ook font-awesome en devdependencies, ngtemplate url's goed

--- grunt/config/ngtemplates.js
+++ grunt/config/ngtemplates.js
@@ -1,8 +1,7 @@
-// werkt nog niet, geen prio later naar kijken, taak faalt om onduidelijke reden
 module.exports = {
     app: {
-        cwd: '<%= app %>',
-        src: '**/*.html',
+        cwd: '<%= app %>/..',
+        src: 'modules/**/*.html',
         dest: 'templates.js',
         options: {
             module: 'atlas',
