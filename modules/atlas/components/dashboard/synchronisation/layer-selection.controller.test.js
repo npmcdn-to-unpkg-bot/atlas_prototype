4a5cda30e05b1f5530008e66a6beae3113073fc4
Erik Nijland <e.n.nijland@gmail.com>
2016-06-13 10:23:51 +0200

    Unit tests for the layer-selection controller.

---
+++ modules/atlas/components/dashboard/synchronisation/layer-selection.controller.test.js
@@ -0,0 +1,55 @@
+describe('The layerSelection controller', function () {
+    var $controller,
+        $rootScope,
+        store;
+
+    beforeEach(function () {
+        angular.mock.module('atlas');
+
+        angular.mock.inject(function (_$controller_, _$rootScope_, _store_) {
+            $controller = _$controller_;
+            $rootScope = _$rootScope_;
+            store = _store_;
+        });
+    });
+
+    function getController () {
+        var controller,
+            scope = $rootScope.$new();
+
+        controller = $controller('LayerSelectionController', {
+            $scope: scope
+        });
+
+        scope.$apply();
+
+        return controller;
+    }
+
+    it('subscribes to the store to listen for changes', function () {
+        spyOn(store, 'subscribe').and.callThrough();
+
+        getController();
+
+        expect(store.subscribe).toHaveBeenCalledWith(jasmine.any(Function));
+    });
+
+    it('sets the baseLayer, overlays and zoom level based on the state', function () {
+        var mockedState = {
+                map: {
+                    baseLayer: 'topografie',
+                    overlays: ['layer_1', 'layer_3'],
+                    zoom: 10
+                }
+            },
+            controller;
+
+        spyOn(store, 'getState').and.returnValue(mockedState);
+
+        controller = getController();
+
+        expect(controller.baseLayer).toBe('topografie');
+        expect(controller.overlays).toEqual(['layer_1', 'layer_3']);
+        expect(controller.zoom).toBe(10);
+    });
+});
