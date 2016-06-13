05bfc5e62b76c2d7c9040bff1278d6dd839feafb
Erik Nijland <e.n.nijland@gmail.com>
2016-06-13 14:55:25 +0200

    ngtemplates config: keeping the templates with the correct module (instead of linking it all to atlas).

---
+++ modules/page/components/page/page.component.test.js
@@ -0,0 +1,40 @@
+fdescribe('The page component', function () {
+    var $compile,
+        $rootScope;
+
+    beforeEach(function () {
+        angular.mock.module('atlasPage');
+
+        angular.mock.inject(function (_$compile_, _$rootScope_) {
+            $compile = _$compile_;
+            $rootScope = _$rootScope_;
+        });
+    });
+
+    function getComponent (name) {
+        var component,
+            element,
+            scope;
+
+        element = document.createElement('atlas-page');
+        element.setAttribute('name', name);
+
+        scope = $rootScope.$new();
+
+        component = $compile(element)(scope);
+        scope.$apply();
+
+        return component;
+    }
+
+    it('loads an HTML page based on the name binding', function () {
+        var component;
+
+        component = getComponent('page_a');
+        console.log(component);
+    });
+
+    it('changes the HTML page when the binding changes', function () {
+
+    });
+});
