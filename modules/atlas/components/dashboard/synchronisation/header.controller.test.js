1cc2cc8f0a6c930a7ff2b861641cf59225df495f
Erik Nijland <e.n.nijland@gmail.com>
2016-06-13 10:19:03 +0200

    Unit tests for the header controller.

---
+++ modules/atlas/components/dashboard/synchronisation/header.controller.test.js
@@ -0,0 +1,64 @@
+describe('The header controller', function () {
+    var $controller,
+        $rootScope,
+        store;
+
+    beforeEach(function () {
+        angular.mock.module('atlas');
+
+        angular.mock.inject(function (_$controller_, _$rootScope_, _store_) {
+            $controller = _$controller_;
+            $rootScope = _$rootScope_;
+            store = _store_;
+        });
+    });
+
+    function getController () {
+        var controller,
+            scope = $rootScope.$new();
+
+        controller = $controller('HeaderController', {
+            $scope: scope
+        });
+
+        scope.$apply();
+
+        return controller;
+    }
+
+    it('subscribes to the store to listen for changes', function () {
+        spyOn(store, 'subscribe').and.callThrough();
+
+        getController();
+
+        expect(store.subscribe).toHaveBeenCalledWith(jasmine.any(Function));
+    });
+
+    it('sets the query string based on the state', function () {
+        var mockedState = {
+                search: {
+                    query: 'i am a search query'
+                }
+            },
+            controller;
+
+        spyOn(store, 'getState').and.returnValue(mockedState);
+
+        controller = getController();
+
+        expect(controller.query).toBe('i am a search query');
+    });
+
+    it('doesn\'t break when search is null', function () {
+        var mockedState = {
+                search: null
+            },
+            controller;
+
+        spyOn(store, 'getState').and.returnValue(mockedState);
+
+        controller = getController();
+
+        expect(controller.query).toBeNull();
+    });
+});
