8c6158c631a752cafc558db1e712cb24b3381bef
Erik Nijland <e.n.nijland@gmail.com>
2016-06-16 16:49:44 +0200

    Unit tests for the new middleware.

---
+++ modules/atlas/services/application-state/redux/middleware/state-to-url-middleware.factory.test.js
@@ -0,0 +1,34 @@
+describe('The stateToUrlMiddleware factory', function () {
+    var stateToUrlMiddleware,
+        mockedStore = {
+            getState: function () {
+                return 'FAKE_STATE';
+            }
+        },
+        mockedNext = function (action) {
+            return action;
+        },
+        mockedAction = 'FAKE_ACTION',
+        stateToUrl;
+
+    beforeEach(function () {
+        angular.mock.module('atlas');
+
+        angular.mock.inject(function (_stateToUrlMiddleware_, _stateToUrl_) {
+            stateToUrlMiddleware = _stateToUrlMiddleware_;
+            stateToUrl = _stateToUrl_;
+        });
+    });
+
+    it('calls the next action', function () {
+        var returnValue;
+
+        spyOn(stateToUrl, 'update');
+
+        returnValue = stateToUrlMiddleware(mockedStore)(mockedNext)(mockedAction);
+
+        expect(stateToUrl.update).toHaveBeenCalledWith('FAKE_STATE');
+
+        expect(returnValue).toBe('FAKE_ACTION');
+    });
+});
