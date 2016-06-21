df7db62368d1c6f9ff1f614cb2ce4e7f265d0845
Tiety Kooistra <tietyk@hotmail.com>
2016-06-21 15:18:27 +0200

    stelselpedia header toevoegen deel een

---
+++ modules/detail/components/stelselpedia/header/stelselpedia-header.directive.js
@@ -0,0 +1,47 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('atlasDetail')
+    .directive('dpStelselpediaHeader', dpStelselpediaHeaderDirective);
+
+  function dpStelselpediaHeaderDirective () {
+    return {
+      restrict: 'E',
+      scope: {
+        heading: '@',
+        definition: '@',
+        usePlural: '=',
+        metaData: '=',
+        brk: '='
+      },
+      templateUrl: 'modules/detail/components/stelselpedia/header/stelselpedia-header.html',
+      transclude: true,
+      controller: DpStelselpediaHeaderController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+  }
+
+  DpStelselpediaHeaderController.$inject = ['$sce', 'STELSELPEDIA'];
+
+  function DpStelselpediaHeaderController ($sce, STELSELPEDIA) {
+    var vm = this,
+      isVisible = {};
+
+    vm.htmlHeading = $sce.trustAsHtml(vm.heading);
+
+    vm.stelselpedia = STELSELPEDIA.DEFINITIONS[vm.definition];
+    vm.stelselpedia.label = vm.usePlural ? vm.stelselpedia.label_plural : vm.stelselpedia.label_singular;
+
+    vm.hasMetaData = angular.isDefined(vm.metaData);
+
+    vm.toggle = function (item) {
+      isVisible[item] = !isVisible[item];
+    };
+
+    vm.isVisible = function (item) {
+      return isVisible[item];
+    };
+  }
+})();
---
+++ modules/detail/components/stelselpedia/header/stelselpedia-header.directive.test.js
@@ -0,0 +1,250 @@
+describe('The dp-stelselpedia-header directive', function () {
+  var $compile,
+    $rootScope;
+
+  beforeEach(function () {
+    angular.mock.module('atlasDetail', function ($provide) {
+      $provide.constant('STELSELPEDIA', {
+        DEFINITIONS: {
+          BOUWBLOK: {
+            label_singular: 'Bouwblok',
+            label_plural: 'Bouwblokken',
+            description: 'Verhaaltje over bouwblokken',
+            url: 'http://www.example.com/bouwblok/',
+            meta: ['begin_geldigheid', 'id']
+          },
+          BRONDOCUMENT: {
+            label_singular: 'Brondocument',
+            label_plural: 'Brondocumenten',
+            description: 'Verhaaltje over brondocumenten.',
+            url: 'http://www.example.com/brondocument/',
+            meta: []
+          },
+          GEMEENTELIJKE_BEPERKING: {
+            label_singular: 'Gemeentelijke beperking',
+            label_plural: 'Gemeentelijke beperkingen',
+            description: 'Lijst van beperkingen op een gebruiksrecht.',
+            url: 'http://www.example.com/gemeentelijkebeperkingen/',
+            meta: []
+          }
+        }
+      });
+
+      $provide.factory('dpStelselpediaMetaDirective', function () {
+        return {};
+      });
+    });
+
+    angular.mock.inject(function (_$compile_, _$rootScope_) {
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+    });
+  });
+
+  function getDirective (heading, definition, usePlural, metaData, brk) {
+    var directive,
+      element,
+      scope;
+
+    element = document.createElement('dp-stelselpedia-header');
+    scope = $rootScope.$new();
+
+    if (heading !== null) {
+      element.setAttribute('heading', heading);
+    }
+
+    element.setAttribute('definition', definition);
+
+    element.setAttribute('use-plural', 'usePlural');
+    scope.usePlural = usePlural;
+
+    if (metaData !== null) {
+      element.setAttribute('meta-data', 'metaData');
+      scope.metaData = metaData;
+    }
+
+    if (brk !== null) {
+      element.setAttribute('brk', 'brk');
+      scope.brk = brk;
+    }
+
+    directive = $compile(element)(scope);
+    $rootScope.$apply();
+
+    return directive;
+  }
+
+  it('always shows the Stelselpedia label w/ an button to toggle more information', function () {
+    var directive = getDirective(null, 'BOUWBLOK', false, null, null);
+
+    expect(directive.find('.stelselpedia-header__title').text().trim()).toBe('Bouwblok');
+    expect(directive.find('.stelselpedia-header__button:nth-of-type(1)').text().trim()).toBe('toon uitleg');
+  });
+
+  it('has support for plurals in the Stelselpedia label', function () {
+    var directive;
+
+    directive = getDirective(null, 'BOUWBLOK', false, null, null);
+    expect(directive.find('.stelselpedia-header__title').text().trim()).toBe('Bouwblok');
+
+    directive = getDirective(null, 'BOUWBLOK', true, null, null);
+    expect(directive.find('.stelselpedia-header__title').text().trim()).toBe('Bouwblokken');
+  });
+
+  it('has an optional heading that can be placed in front of the Stelselpedia label', function () {
+    var directive = getDirective('Ik ben een hele specifieke titel', 'BOUWBLOK', false, null);
+
+    expect(directive.find('.stelselpedia-header__title span').eq(0).text())
+      .toBe('Ik ben een hele specifieke titel -');
+
+    expect(directive.find('.stelselpedia-header__title span').eq(2).attr('class'))
+      .toContain('stelselpedia-header__subtitle');
+
+    expect(directive.find('.stelselpedia-header__title span').eq(2).text().trim()).toBe('Bouwblok');
+  });
+
+  it('always has a Stelselpedia panel', function () {
+    var directive = getDirective(null, 'BOUWBLOK', false, null, null);
+
+    //The panel is hidden by default
+    expect(directive.find('.stelselpedia-header__content').length).toBe(0);
+
+    //The panel can be opened by clicking the 'toon uitleg' button
+    directive.find('.stelselpedia-header__button:nth-of-type(1)').click();
+    expect(directive.find('.stelselpedia-header__content').length).toBe(1);
+
+    //Inside the content of the panel
+    expect(directive.find('.stelselpedia-header__content h3').text()).toBe('Uitleg over bouwblok');
+    expect(directive.find('.stelselpedia-header__content p:nth-of-type(1)').text()).toBe('Verhaaltje over bouwblokken');
+    expect(directive.find('.stelselpedia-header__content p:nth-of-type(2) a').attr('href'))
+      .toBe('http://www.example.com/bouwblok/');
+    expect(directive.find('.stelselpedia-header__content p:nth-of-type(2) a').text().trim())
+      .toBe('Lees verder op stelselpedia');
+  });
+
+  describe('optionally loads the dp-stelselpedia-meta directive', function () {
+    var metaData;
+
+    beforeEach(function () {
+      metaData = {
+        id: '123456',
+        begin_geldigheid: '2016-03-30T22:00:32.017685Z'
+      };
+    });
+
+    it('optionally show a button with \'toon metadata\' in the header', function () {
+      var directive;
+
+      //BRONDOCUMENT has no meta data
+      directive = getDirective(null, 'BRONDOCUMENT', false, null, null);
+
+      expect(directive.find('.stelselpedia-header__button:nth-of-type(2)').length).toBe(0);
+
+      //BOUWBLOK has metadata
+      directive = getDirective(null, 'BOUWBLOK', false, metaData, null);
+      expect(directive.find('.stelselpedia-header__button:nth-of-type(2)').length).toBe(1);
+      expect(directive.find('.stelselpedia-header__button:nth-of-type(2)').text().trim()).toBe('toon metadata');
+    });
+
+    it('can open a panel that loads the dp-stelselpedia-meta directive', function () {
+      var directive = getDirective(null, 'BOUWBLOK', false, metaData, null);
+
+      //The panel is hidden by default
+      expect(directive.find('.stelselpedia-header__content').length).toBe(0);
+
+      //Open the panel
+      directive.find('.stelselpedia-header__button:nth-of-type(2)').click();
+
+      expect(directive.find('.stelselpedia-header__content').length).toBe(1);
+      expect(directive.find('.stelselpedia-header__content h3').text().trim()).toBe('Metadata van bouwblok');
+      expect(directive.find('.stelselpedia-header__content dp-stelselpedia-meta').length).toBe(1);
+    });
+  });
+
+  describe('optionally activates the dp-wkpb-link directive', function () {
+    var brk;
+
+    beforeEach(function () {
+      brk = {};
+    });
+
+    it('optionally show a button with \'WKPB-uittreksel\' in the header als GEMEENTELIJKE_BEPERKING', function () {
+      var directive;
+
+      //BRONDOCUMENT has no wkpb-uittreksel
+      directive = getDirective(null, 'BRONDOCUMENT', false, null, null);
+
+      expect(directive.find('.stelselpedia-header__button:nth-of-type(2)').length).toBe(0);
+
+      //GEMEENTELIJKE_BEPERKING enige met wkpb uittreksel
+      directive = getDirective(null, 'GEMEENTELIJKE_BEPERKING', false, null, brk);
+      expect(directive.find('dp-wkpb-link').length).toBe(1);
+      expect(directive.find('dp-wkpb-link a').text().trim()).toBe('WKPB-uittreksel');
+    });
+
+  });
+
+  describe('the show more stuff buttons', function () {
+    var directive,
+      metaData = {
+        id: '123456',
+        begin_geldigheid: '2016-03-30T22:00:32.017685Z'
+      };
+
+    beforeEach(function () {
+      directive = getDirective(null, 'BOUWBLOK', false, metaData, null);
+    });
+
+    it('can be opened and closed with the button in the header', function () {
+      expect(directive.find('.stelselpedia-header__content').length).toBe(0);
+
+      //Open uitleg
+      directive.find('.stelselpedia-header__button:nth-of-type(1)').click();
+      expect(directive.find('.stelselpedia-header__content').length).toBe(1);
+
+      //Close uitleg with the button in the header
+      directive.find('.stelselpedia-header__button:nth-of-type(1)').click();
+      expect(directive.find('.stelselpedia-header__content').length).toBe(0);
+
+      //Open metadata
+      directive.find('.stelselpedia-header__button:nth-of-type(2)').click();
+      expect(directive.find('.stelselpedia-header__content').length).toBe(1);
+
+      //Close metadata with the button in the header
+      directive.find('.stelselpedia-header__button:nth-of-type(2)').click();
+      expect(directive.find('.stelselpedia-header__content').length).toBe(0);
+    });
+
+    it('can be closed with the button (cross) in the panel', function () {
+      expect(directive.find('.stelselpedia-header__content').length).toBe(0);
+
+      //Open uitleg
+      directive.find('.stelselpedia-header__button:nth-of-type(1)').click();
+      expect(directive.find('.stelselpedia-header__content').length).toBe(1);
+
+      //Close uitleg with the cross
+      directive.find('.stelselpedia-header__content .stelselpedia-header__button').click();
+      expect(directive.find('.stelselpedia-header__content').length).toBe(0);
+
+      //Open metadata
+      directive.find('.stelselpedia-header__button:nth-of-type(2)').click();
+      expect(directive.find('.stelselpedia-header__content').length).toBe(1);
+
+      //Close metadata with the cross
+      directive.find('.stelselpedia-header__content .stelselpedia-header__button').click();
+      expect(directive.find('.stelselpedia-header__content').length).toBe(0);
+    });
+
+    it('can open uitleg and metadata at the same time', function () {
+      expect(directive.find('.stelselpedia-header__content').length).toBe(0);
+
+      //Open uitleg
+      directive.find('.stelselpedia-header__button:nth-of-type(1)').click();
+      expect(directive.find('.stelselpedia-header__content').length).toBe(1);
+
+      //Open metadata
+      directive.find('.stelselpedia-header__button:nth-of-type(2)').click();
+      expect(directive.find('.stelselpedia-header__content').length).toBe(2);
+    });
+  });
+});
