268235be7298ef47564f125ac7b92bb28d204a67
Erik Nijland <e.n.nijland@gmail.com>
2016-06-22 15:49:56 +0200

    crs basics are in place

--- modules/map/services/crs/crs.service.js
+++ modules/map/services/crs/crs.service.js
@@ -1,68 +1,63 @@
 (function(){
-  'use strict';
+    'use strict';
 
-  angular
-    .module('dpMap')
-    .service('crsService', crsService);
+    angular
+        .module('dpMap')
+        .service('crsService', crsService);
 
-  crsService.$inject = ['L', 'CRS_CONFIG'];
+    crsService.$inject = ['L', 'CRS_CONFIG'];
 
-  function crsService (L, CRS_CONFIG) {
-    var rd;
+    function crsService (L, CRS_CONFIG) {
+        var rd;
 
-    activate();
+        activate();
 
-    return {
-      'getRd': getRd,
-      'getRdObject': getRdObject,
-      'getWgs84Code': getWgs84Code
-    };
+        return {
+            'getRd': getRd,
+            'getRdObject': getRdObject
+        };
 
-    function activate () {
-      var rdSettings;
+        function activate () {
+            var rdSettings;
 
-      rdSettings = angular.copy(CRS_CONFIG.RD);
+            rdSettings = angular.copy(CRS_CONFIG.RD);
 
-      /*
-       * Convert the Array syntax from the configuration to the L.bounds format
-       * http://leafletjs.com/reference.html#bounds
-       */
-      rdSettings.transformation.bounds = L.bounds.apply(null, rdSettings.transformation.bounds);
+            /*
+             * Convert the Array syntax from the configuration to the L.bounds format
+             * http://leafletjs.com/reference.html#bounds
+             */
+            rdSettings.transformation.bounds = L.bounds.apply(null, rdSettings.transformation.bounds);
 
-      rd = new L.Proj.CRS(
-        rdSettings.code,
-        rdSettings.projection,
-        rdSettings.transformation
-      );
+            rd = new L.Proj.CRS(
+                rdSettings.code,
+                rdSettings.projection,
+                rdSettings.transformation
+            );
 
-      /*
-       * Door bug in Leaflet werkt de schaalbalk niet met de standaard RD instellingen er worden
-       * voor een workaround hier 2 waarden toegevoegd. initiatie schaal in zoom.directive.js
-       * https://github.com/Leaflet/Leaflet/issues/4091
-       */
-      rd.distance = L.CRS.Earth.distance;
-      rd.R = CRS_CONFIG.EARTH_RADIUS;
-    }
-
-    function getRd () {
-      return rd;
-    }
-
-    /*
-     * @returns {Object} - An object used to identify the CRS of a GeoJSON object:
-     * http://geojson.org/geojson-spec.html#coordinate-reference-system-objects
-     */
-    function getRdObject () {
-      return {
-        'type': 'name',
-        'properties': {
-          'name': CRS_CONFIG.RD.code
+            /*
+             * Door bug in Leaflet werkt de schaalbalk niet met de standaard RD instellingen er worden
+             * voor een workaround hier 2 waarden toegevoegd. initiatie schaal in zoom.directive.js
+             * https://github.com/Leaflet/Leaflet/issues/4091
+             */
+            rd.distance = L.CRS.Earth.distance;
+            rd.R = CRS_CONFIG.EARTH_RADIUS;
         }
-      };
-    }
 
-    function getWgs84Code () {
-      return CRS_CONFIG.WGS84.code;
+        function getRd () {
+            return rd;
+        }
+
+        /*
+         * @returns {Object} - An object used to identify the CRS of a GeoJSON object:
+         * http://geojson.org/geojson-spec.html#coordinate-reference-system-objects
+         */
+        function getRdObject () {
+            return {
+                type: 'name',
+                properties: {
+                    name: CRS_CONFIG.RD.code
+                }
+            };
+        }
     }
-  }
 })();
--- modules/map/services/crs/crs.service.test.js
+++ modules/map/services/crs/crs.service.test.js
@@ -1,41 +1,38 @@
 describe('The crsService', function () {
-  var crsService,
-    CRS_CONFIG;
+    var crsService,
+        CRS_CONFIG;
 
-  beforeEach(function () {
-    angular.mock.module('dpMap', {
-      CRS_CONFIG: {
-        RD: {
-          code: 'a',
-          projection: 'b',
-          transformation: {
-            bounds: 'c'
-          }
-        },
-        WGS84: {
-          code: 'd'
-        },
-        EARTH_RADIUS: 123456789
-      }
+    beforeEach(function () {
+        angular.mock.module('dpMap', {
+            CRS_CONFIG: {
+                RD: {
+                    code: 'a',
+                    projection: 'b',
+                    transformation: {
+                        bounds: 'c'
+                    }
+                },
+                WGS84: {
+                    code: 'd'
+                },
+                EARTH_RADIUS: 123456789
+            }
+        });
+
+        angular.mock.inject(function (_crsService_, _CRS_CONFIG_) {
+            crsService = _crsService_;
+            CRS_CONFIG = _CRS_CONFIG_;
+        });
     });
 
-    angular.mock.inject(function (_crsService_, _CRS_CONFIG_) {
-      crsService = _crsService_;
-      CRS_CONFIG = _CRS_CONFIG_;
+    it('may not alter the configuration source', function () {
+        //This specification solves ATLAS-1101
+        var originalConfig = angular.copy(CRS_CONFIG);
+
+        crsService.getRd();
+        expect(CRS_CONFIG).toEqual(originalConfig);
+
+        crsService.getRdObject();
+        expect(CRS_CONFIG).toEqual(originalConfig);
     });
-  });
-
-  it('may not alter the configuration source', function () {
-    //This specification solves ATLAS-1101
-    var originalConfig = angular.copy(CRS_CONFIG);
-
-    crsService.getRd();
-    expect(CRS_CONFIG).toEqual(originalConfig);
-
-    crsService.getRdObject();
-    expect(CRS_CONFIG).toEqual(originalConfig);
-
-    crsService.getWgs84Code();
-    expect(CRS_CONFIG).toEqual(originalConfig);
-  });
 });
