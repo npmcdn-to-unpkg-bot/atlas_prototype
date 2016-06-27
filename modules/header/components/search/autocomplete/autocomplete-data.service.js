2cf4e58970dd39740b0aeffc185d72b77fde23f1
Erik Nijland <e.n.nijland@gmail.com>
2016-06-27 16:06:15 +0200

    Autocomplete now using proper configuration instead of hardcoded links.

--- modules/header/components/search/autocomplete/autocomplete-data.service.js
+++ modules/header/components/search/autocomplete/autocomplete-data.service.js
@@ -1,76 +1,75 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('atlasHeader')
-    .service('autocompleteData', autocompleteDataService);
+    angular
+        .module('atlasHeader')
+        .service('autocompleteData', autocompleteDataService);
 
-  autocompleteDataService.$inject = ['$q', '$http'];
+    autocompleteDataService.$inject = ['$q', '$http', 'environment', 'HEADER_CONFIG'];
 
-  function autocompleteDataService ($q, $http) {
-    var request;
+    function autocompleteDataService ($q, $http, environment, HEADER_CONFIG) {
+        var request;
 
-    return {
-      search: search,
-      cancel: cancel,
-      getSuggestionByIndex: getSuggestionByIndex
-    };
+        return {
+            search: search,
+            cancel: cancel,
+            getSuggestionByIndex: getSuggestionByIndex
+        };
 
-    function search (query) {
-      //An $http call can't be aborted, hence the $q promise and it's reject method
-      request = $q.defer();
+        function search (query) {
+            //An $http call can't be aborted, hence the $q promise and it's reject method
+            request = $q.defer();
 
-      $http({
-        method: 'GET',
-        url: 'https://api.datapunt.amsterdam.nl/atlas/typeahead/',
-        //url: 'http://eriknijland.nl/atlas_autocomplete/eriks_nep_autocomplete.php',
-        params: {
-          q: query
+            $http({
+                method: 'GET',
+                url: environment.API_ROOT + HEADER_CONFIG.AUTOCOMPLETE_PATH,
+                params: {
+                    q: query
+                }
+            }).then(function (response) {
+                request.resolve(formatData(response.data));
+            });
+
+            return request.promise;
         }
-      }).then(function (response) {
-        request.resolve(formatData(response.data));
-      });
 
-      return request.promise;
+        function formatData (categories) {
+            var suggestionIndex = 0,
+                numberOfResults = 0;
+
+            categories.forEach(function (category) {
+                category.content.map(function (suggestion) {
+                    suggestion.index = suggestionIndex++;
+                    numberOfResults++;
+
+                    return suggestion;
+                });
+            });
+
+            return {
+                count: numberOfResults,
+                data: categories
+            };
+        }
+
+        function cancel () {
+            if (angular.isDefined(request)) {
+                request.reject('cancelled');
+            }
+        }
+
+        function getSuggestionByIndex (searchResults, index) {
+            var activeSuggestion;
+
+            searchResults.forEach(function (category) {
+                category.content.forEach(function (suggestion) {
+                    if (suggestion.index === index) {
+                        activeSuggestion = suggestion;
+                    }
+                });
+            });
+
+            return activeSuggestion;
+        }
     }
-
-    function formatData (categories) {
-      var suggestionIndex = 0,
-        numberOfResults = 0;
-
-      categories.forEach(function (category) {
-        category.content.map(function (suggestion) {
-          suggestion.index = suggestionIndex++;
-          numberOfResults++;
-
-          return suggestion;
-        });
-      });
-
-      return {
-        count: numberOfResults,
-        data: categories
-      };
-    }
-
-    function cancel () {
-      if (angular.isDefined(request)) {
-        request.reject('cancelled');
-      }
-    }
-
-    function getSuggestionByIndex (searchResults, index) {
-      var activeSuggestion;
-
-      searchResults.forEach(function (category) {
-        category.content.forEach(function (suggestion) {
-          if (suggestion.index === index) {
-            activeSuggestion = suggestion;
-          }
-        });
-      });
-
-      return activeSuggestion;
-    }
-  }
 })();
