8ef454d7efd1a71dd1e1958497bc36ddd2621244
Erik Nijland <e.n.nijland@gmail.com>
2016-06-29 11:15:17 +0200

    Unit tests for the header.component. Checking it's inclusion of atlas-search and opening of pages through it's navigation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('build-develop', [
         'clean:build',
+
         'copy:index',
         'copy:assets',
 
--- modules/header/components/header/header.component.js
+++ modules/header/components/header/header.component.js
@@ -23,10 +23,6 @@
                 payload: name
             });
         };
-
-        vm.showLoginModal = function () {
-
-        };
     }
 })();
 
