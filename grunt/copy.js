7ca04d8f8c22f63a81de63349f42a798e2091ca5
Erik Nijland <e.n.nijland@gmail.com>
2016-06-29 10:54:33 +0200

    Very basic way to copy assets from the source to the build folder. It assumes unique filenames for each asset regardless of the module.

--- grunt/copy.js
+++ grunt/copy.js
@@ -2,5 +2,15 @@ module.exports = {
     index: {
         src: ['index.html'],
         dest: 'build/index.html'
+    },
+    assets: {
+        files: [
+            {
+                src: ['modules/*/assets/*.png'],
+                dest: 'build/assets/',
+                expand: true,
+                flatten: true
+            }
+        ]
     }
 };
