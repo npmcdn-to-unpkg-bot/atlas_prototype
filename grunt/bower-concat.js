1fd4596a64fabcf9e6fdd5be5c6d6ca3c261d4ed
Erik Nijland <e.n.nijland@gmail.com>
2016-07-08 16:22:18 +0200

    Added i18n

--- grunt/bower-concat.js
+++ grunt/bower-concat.js
@@ -1,5 +1,8 @@
 module.exports = {
     js: {
+        mainFiles: {
+            'angular-i18n': ['angular-locale_nl-nl.js']
+        },
         dest: {
             js: 'build/temp/bower_components.js'
         }
