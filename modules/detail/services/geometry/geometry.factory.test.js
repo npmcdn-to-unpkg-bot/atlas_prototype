4d4c93b379a0d10bbd149d8f10611dc980d0003f
Erik Nijland <e.n.nijland@gmail.com>
2016-07-18 15:08:25 +0200

    Fixes for geometry unit tests. Old atlas_client test code was bullshit. All mocked endpoints returned the same result, making tests for each scenario worthless.

--- modules/detail/services/geometry/geometry.factory.test.js
+++ modules/detail/services/geometry/geometry.factory.test.js
@@ -1,183 +1,192 @@
-describe('The geometry factory', function () {
-    var $httpBackend,
-        $q,
+fdescribe('The geometry factory', function () {
+    var $q,
         $rootScope,
         geometry,
-        fakeGeoJSON = {
-            foo: 'bar',
-            whatever: true
-        },
-        mockedData = {
-            gPerceel: {
+        mockedData = [
+            {
+                //G Perceel
                 url: 'http://www.api-root.nl/brk/object/NL.KAD.OnroerendeZaak.123456/',
                 response: {
-                    geometrie: fakeGeoJSON
+                    geometrie: {
+                        FAKE_GEOJSON: 'G_PERCEEL'
+                    }
                 }
-            },
-            aPerceel: {
+            }, {
+                //A Perceel
                 url: 'http://www.api-root.nl/brk/object/NL.KAD.OnroerendeZaak.456789/',
                 response: {
                     index_letter: 'A',
                     g_percelen: {
-                        href: 'http://mocked-atlas-api.amsterdam.nl/brk/object/?a_percelen__id=NL.KAD.OnroerendeZaak.123456'
+                        href: 'http://mocked-atlas-api.amsterdam.nl/brk/object/?a_percelen__id=NL.KAD.OnroerendeZaak.' +
+                            '123456'
                     }
                 }
-            },
-            gPercelen: {
+            }, {
+                //G Percelen (meervoud)
                 url: 'http://mocked-atlas-api.amsterdam.nl/brk/object/?a_percelen__id=NL.KAD.OnroerendeZaak.123456',
                 response: {
                     results: [{
                         _links: {
                             self: {
-                                href: 'http://mocked-atlas-api.amsterdam.nl/brk/object/NL.KAD.OnroerendeZaak.123456/'
+                                href: 'http://www.api-root.nl/brk/object/NL.KAD.OnroerendeZaak.123456/'
                             }
                         }
                     }]
                 }
-            },
-            nummeraanduidingVerblijfsobject: {
-                url: 'http://www.api-root.nl/bag/nummeraanduiding/123456/',
+            }, {
+                //Nummeraanduiding - verblijfsobject
+                url: 'http://www.api-root.nl/bag/nummeraanduiding/10000001/',
                 response: {
-                    type: 'Verbijlfsobject',
-                    verbijlfsobject: 'http://mocked-atlas-api.amsterdam.nl/bag/verblijfsobject/03630000553603/'
+                    type: 'Verblijfsobject',
+                    verblijfsobject: 'http://mocked-atlas-api.amsterdam.nl/bag/verblijfsobject/20000001/'
                 }
-            },
-            verbijlfsobject:{
-                url: 'http://mocked-atlas-api.amsterdam.nl/bag/verblijfsobject/03630000553603/',
+            }, {
+                //Verblijfsobject
+                url: 'http://mocked-atlas-api.amsterdam.nl/bag/verblijfsobject/20000001/',
                 response: {
-                    geometrie: fakeGeoJSON
+                    geometrie: {
+                        FAKE_GEOJSON: 'VERBLIJFSOBJECT'
+                    }
                 }
-            },
-            nummeraanduidingLigplaats: {
-                url: 'http://www.api-root.nl/bag/nummeraanduiding/123456/',
+            }, {
+                //Nummeraanduiding - ligplaats
+                url: 'http://www.api-root.nl/bag/nummeraanduiding/10000002/',
                 response: {
                     type: 'Ligplaats',
-                    ligplaats: 'http://mocked-atlas-api.amsterdam.nl/bag/ligplaats/03630001030154/'
+                    ligplaats: 'http://mocked-atlas-api.amsterdam.nl/bag/ligplaats/20000002/'
                 }
-            },
-            ligplaats:{
-                url: 'http://mocked-atlas-api.amsterdam.nl/bag/ligplaats/03630001030154/',
+            }, {
+                //Ligplaats
+                url: 'http://mocked-atlas-api.amsterdam.nl/bag/ligplaats/20000002/',
                 response: {
-                    geometrie: fakeGeoJSON
+                    geometrie: {
+                        FAKE_GEOJSON: 'LIGPLAATS'
+                    }
                 }
-            },
-            nummeraanduidingStandplaats: {
-                url: 'http://www.api-root.nl/bag/nummeraanduiding/123456/',
+            }, {
+                //Nummeraanduiding - standplaats
+                url: 'http://www.api-root.nl/bag/nummeraanduiding/10000003/',
                 response: {
                     type: 'Standplaats',
-                    standplaats: 'http://mocked-atlas-api.amsterdam.nl/bag/standplaats/03630000691674/'
+                    standplaats: 'http://mocked-atlas-api.amsterdam.nl/bag/standplaats/20000001/'
                 }
-            },
-            standplaats: {
-                url: 'http://mocked-atlas-api.amsterdam.nl/bag/standplaats/03630000691674/',
+            }, {
+                //Standplaats
+                url: 'http://mocked-atlas-api.amsterdam.nl/bag/standplaats/20000001/',
                 response: {
-                    geometrie: fakeGeoJSON
+                    geometrie: {
+                        FAKE_GEOJSON: 'STANDPLAATS'
+                    }
                 }
-            },
-            kadastraalSubject: {
+            }, {
+                //Kadastraal subject
                 url: 'http://www.api-root.nl/brk/subject/NL.KAD.Persoon.123456/',
-                response: {
-                }
+                response: {}
             }
-        };
+        ];
 
     beforeEach(function () {
-        angular.mock.module('atlasDetail');
+        angular.mock.module(
+            'atlasDetail',
+            {
+                api: {
+                    getByUrl: function (url) {
+                        var q = $q.defer(),
+                            response;
 
-        angular.mock.inject(function (_$httpBackend_, _$q_, _$rootScope_, _geometry_) {
-            $httpBackend = _$httpBackend_;
+                        response = mockedData.filter(function (item) {
+                            return item.url === url;
+                        })[0].response;
+
+                        q.resolve(response);
+
+                        return q.promise;
+                    }
+                }
+            }
+        );
+
+        angular.mock.inject(function (_$q_, _$rootScope_, _geometry_) {
             $q = _$q_;
             $rootScope = _$rootScope_;
             geometry = _geometry_;
         });
-
-        $httpBackend.whenGET(mockedData.gPerceel.url).respond(mockedData.gPerceel.response);
-        $httpBackend.whenGET(mockedData.aPerceel.url).respond(mockedData.aPerceel.response);
-        $httpBackend.whenGET(mockedData.gPercelen.url).respond(mockedData.gPercelen.response);
-        $httpBackend.whenGET('http://mocked-atlas-api.amsterdam.nl/brk/object/NL.KAD.OnroerendeZaak.123456/')
-            .respond(mockedData.gPerceel.response);
-
-        $httpBackend.whenGET(mockedData.nummeraanduidingVerblijfsobject.url)
-            .respond(mockedData.nummeraanduidingVerblijfsobject.response);
-        $httpBackend.whenGET(mockedData.verbijlfsobject.url)
-            .respond(mockedData.verbijlfsobject.response);
-
-        $httpBackend.whenGET(mockedData.nummeraanduidingLigplaats.url)
-            .respond(mockedData.nummeraanduidingLigplaats.response);
-        $httpBackend.whenGET(mockedData.ligplaats.url)
-            .respond(mockedData.ligplaats.response);
-
-        $httpBackend.whenGET(mockedData.nummeraanduidingStandplaats.url)
-            .respond(mockedData.nummeraanduidingStandplaats.response);
-        $httpBackend.whenGET(mockedData.standplaats.url)
-            .respond(mockedData.standplaats.response);
-
-        $httpBackend.whenGET(mockedData.kadastraalSubject.url).respond(mockedData.kadastraalSubject.response);
     });
 
     it('returns the GeoJSON of an URL', function () {
         var response;
-        
-        geometry.getGeoJSON(mockedData.gPerceel.url).then(function (data) {
+
+        geometry.getGeoJSON('http://www.api-root.nl/brk/object/NL.KAD.OnroerendeZaak.123456/').then(function (data) {
             response = data;
         });
 
-        $httpBackend.flush();
+        $rootScope.$apply();
 
-        expect(response).toEqual(fakeGeoJSON);
-    });
-
-    describe('when searching for the geometry of an A perceel', function () {
-        it('returns the GeoJSON of the parent G perceel', function () {
-            var response;
-
-            geometry.getGeoJSON(mockedData.aPerceel.url).then(function (data) {
-                response = data;
-            });
-
-            $httpBackend.flush();
-
-            expect(response).toEqual(fakeGeoJSON);
+        expect(response).toEqual({
+            FAKE_GEOJSON: 'G_PERCEEL'
         });
     });
 
-    describe('when searching for the geometry of an nummeraanduiding', function () {
-        describe('returns the GeoJSON of the parent adressseerbaar object', function(){
+    describe('when searching for the geometry of an A perceel', function () {
+        fit('returns the GeoJSON of the parent G perceel', function () {
+            var response;
 
-            it('should return the verbijlfsobject geometry when the type of nummeraanduidingis Verbijlfsobject', function(){
-                var response;
-
-                geometry.getGeoJSON(mockedData.nummeraanduidingVerblijfsobject.url).then(function (data) {
+            geometry
+                .getGeoJSON('http://www.api-root.nl/brk/object/NL.KAD.OnroerendeZaak.456789/')
+                .then(function (data) {
                     response = data;
                 });
 
-                $httpBackend.flush();
+            $rootScope.$apply();
 
-                expect(response).toEqual(fakeGeoJSON);
+            expect(response).toEqual({
+                FAKE_GEOJSON: 'G_PERCEEL'
+            });
+        });
+    });
+
+    describe('when searching for the geometry of a nummeraanduiding', function () {
+        describe('returns the GeoJSON of the parent adressseerbaar object', function(){
+            it('should return the verblijfsobject geometry when the type of nummeraanduiding is verblijfsobject', function(){
+                var response;
+
+                geometry.getGeoJSON('http://www.api-root.nl/bag/nummeraanduiding/10000001/').then(function (data) {
+                    response = data;
+                });
+
+                $rootScope.$apply();
+
+                expect(response).toEqual({
+                    FAKE_GEOJSON: 'VERBLIJFSOBJECT'
+                });
             });
 
             it('should return the ligplaats geometry when the type of nummeraanduiding is Ligplaats', function () {
                 var response;
 
-                geometry.getGeoJSON(mockedData.nummeraanduidingLigplaats.url).then(function (data) {
+                geometry.getGeoJSON('http://www.api-root.nl/bag/nummeraanduiding/10000002/').then(function (data) {
                     response = data;
                 });
 
-                $httpBackend.flush();
+                $rootScope.$apply();
 
-                expect(response).toEqual(fakeGeoJSON);
+                expect(response).toEqual({
+                    FAKE_GEOJSON: 'LIGPLAATS'
+                });
             });
 
             it('should return the standplaats geometry when the type of nummeraanduiding is Standplaats', function(){
                 var response;
 
-                geometry.getGeoJSON(mockedData.nummeraanduidingStandplaats.url).then(function (data) {
+                geometry.getGeoJSON('http://www.api-root.nl/bag/nummeraanduiding/10000003/').then(function (data) {
                     response = data;
                 });
 
-                $httpBackend.flush();
+                $rootScope.$apply();
 
-                expect(response).toEqual(fakeGeoJSON);
+                expect(response).toEqual({
+                    FAKE_GEOJSON: 'STANDPLAATS'
+                });
             });
 
         });
@@ -187,11 +196,11 @@ describe('The geometry factory', function () {
     it('will return null if there is no geometry available', function () {
         var response;
 
-        geometry.getGeoJSON(mockedData.kadastraalSubject.url).then(function (data) {
+        geometry.getGeoJSON('http://www.api-root.nl/brk/subject/NL.KAD.Persoon.123456/').then(function (data) {
             response = data;
         });
 
-        $httpBackend.flush();
+        $rootScope.$apply();
 
         expect(response).toBeNull();
     });
