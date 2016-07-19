78475aadd703044dcf4a9879ecdacce68463d37c
Erik Nijland <e.n.nijland@gmail.com>
2016-07-19 13:38:23 +0200

    Unit test coverage naar 90%.

--- karma.conf.js
+++ karma.conf.js
@@ -48,10 +48,10 @@ module.exports = function (config) {
             dir: 'reports/coverage/',
             check: {
                 global: {
-                    statements: 80,
-                    branches: 80,
-                    functions: 80,
-                    lines: 80
+                    statements: 90,
+                    branches: 90,
+                    functions: 90,
+                    lines: 90
                 }
             }
         },
