5ce5cdac2d9fc3f522051f66e9d9476fdae51862
Erik Nijland <e.n.nijland@gmail.com>
2016-07-19 15:35:24 +0200

    wkpb-link unit tests plus simplified the code.

---
+++ modules/detail/components/wkpb-link/wkpb-link.component.test.js
@@ -0,0 +1,48 @@
+describe('The dp-wkpb-link directive', function () {
+    var $compile,
+        $rootScope;
+
+    beforeEach(function () {
+        angular.mock.module(
+            'atlasDetail',
+            {
+                environment: {
+                    API_ROOT: 'http://www.api-root.com/'
+                }
+            },
+            function ($provide) {
+                $provide.factory('dpLinkDirective', function () {
+                    return {};
+                });
+            }
+        );
+
+        angular.mock.inject(function (_$compile_, _$rootScope_) {
+            $compile = _$compile_;
+            $rootScope = _$rootScope_;
+        });
+    });
+
+    function getComponent (brkId) {
+        var component,
+            element,
+            scope;
+
+        element = document.createElement('atlas-wkpb-link');
+        element.setAttribute('brk-id', brkId);
+
+        scope = $rootScope.$new();
+
+        component = $compile(element)(scope);
+        scope.$apply();
+
+        return component;
+    }
+
+    it('creates a dp-link that triggers FETCH_DETAIL to a object-wkpb endpoint', function () {
+        var component = getComponent('abc789');
+
+        expect(component.find('dp-link').attr('type')).toBe('FETCH_DETAIL');
+        expect(component.find('dp-link').attr('payload')).toBe('http://www.api-root.com/brk/object-wkpb/abc789/');
+    });
+});
