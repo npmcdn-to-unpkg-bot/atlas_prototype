ed656e3da7c959079a2a0d23e2cf58e0383bc0e9
Erik Nijland <e.n.nijland@gmail.com>
2016-07-19 17:27:24 +0200

    Converted atlas-api-call from a directive to a component.

---
+++ modules/detail/components/api-call/atlas-api-call.component.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('atlasDetail')
+        .component('atlasApiCall', {
+            bindings: {
+                endpoint: '@',
+                expand: '@',
+                partial: '@'
+            },
+            templateUrl: 'modules/detail/components/api-call/atlas-api-call.html',
+            controller: AtlasApiCallController,
+            controllerAs: 'vm'
+        });
+
+    AtlasApiCallController.$inject = ['api'];
+
+    function AtlasApiCallController (api) {
+        var vm = this;
+
+        if (!vm.endpoint) {
+            return;
+        }
+
+        vm.apiData = {};
+
+        //vraag als true de api-expand resultaten op van BRK
+        if (vm.expand) {
+            vm.endpoint = vm.endpoint.replace('brk/object', 'brk/object-expand');
+        }
+
+        api.getByUrl(vm.endpoint).then(function (response) {
+            vm.apiData.count = response.count;
+
+            if(response._links.next) {
+                vm.apiData.next = response._links.next.href;
+            }
+
+            if(response.results) { //VBO & nummeraanduiding
+                vm.apiData.results = response.results;
+            } else {
+                vm.apiData.results = response;
+            }
+        });
+    }
+})();
