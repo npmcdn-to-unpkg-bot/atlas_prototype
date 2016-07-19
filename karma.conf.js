0df07d5278e9e05fb801edd31b8eaaccb86b273e
Erik Nijland <e.n.nijland@gmail.com>
2016-07-19 17:11:31 +0200

    coverage to 95%! ill stop now..

--- karma.conf.js
+++ karma.conf.js
@@ -48,10 +48,10 @@ module.exports = function (config) {
             dir: 'reports/coverage/',
             check: {
                 global: {
-                    statements: 90,
-                    branches: 90,
-                    functions: 90,
-                    lines: 90
+                    statements: 95,
+                    branches: 95,
+                    functions: 95,
+                    lines: 95
                 }
             }
         },
