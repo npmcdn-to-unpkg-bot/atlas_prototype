4ee161541fea48b6a502710b5c0acd04f4e0fc84
Erik Nijland <e.n.nijland@gmail.com>
2016-07-20 13:37:55 +0200

    Had to make the $compile code asynchronous. We need to wait for Angular's digest cycle to do it's thing.

--- modules/detail/components/partial-select/atlas-partial-select.directive.js
+++ modules/detail/components/partial-select/atlas-partial-select.directive.js
@@ -49,16 +49,23 @@
 
             function getCompiledHtml (templateUrl, data) {
                 return $templateRequest(templateUrl).then(function (template) {
-                    var html,
+                    var q,
                         scope;
+
+                    q = $q.defer();
 
                     scope = $rootScope.$new();
                     scope.apiData = data;
 
-                    html = $compile(template)(scope);
-                    //scope.$apply();
+                    scope.$applyAsync(function () {
+                        /*
+                        Wait for the next digest cycle (making this function asynchronous), the variables should be
+                        rendered inside the template before returning the HTML.
+                        */
+                        q.resolve($compile(template)(scope));
+                    });
 
-                    return html;
+                    return q.promise;
                 });
             }
         }
