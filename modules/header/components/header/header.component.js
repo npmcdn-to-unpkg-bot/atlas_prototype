d8954b55c88dc43f3e43af94160d6dfc69b16ef0
Tiety Kooistra <tietyk@hotmail.com>
2016-07-25 13:01:58 +0200

    opzet dropdown

--- modules/header/components/header/header.component.js
+++ modules/header/components/header/header.component.js
@@ -16,12 +16,18 @@
 
     function AtlasHeaderController (store, ACTIONS) {
         var vm = this;
+        vm.isVisible = true;
 
         vm.showPage = function (name) {
             store.dispatch({
                 type: ACTIONS.SHOW_PAGE,
                 payload: name
             });
+        };
+
+        vm.toggle = function() {
+            vm.isVisible = !vm.isVisible;
+            console.log('hier?');
         };
     }
 })();
--- modules/header/components/menu/dropdown-menu.directive.js
+++ modules/header/components/menu/dropdown-menu.directive.js
@@ -1,37 +1,25 @@
-/*                                                                          *
- * <share-and-extras/> This directive will add a linkbutton to the element. *
- *                                                                          *
- * When clicked on the element a menu will be activated                     *
- * Attributes:                                                              *
- * menu-items: print,save,measure,share                                     *
- *                                                                          */
 (function () {
     'use strict';
 
     angular
         .module('atlasHeader')
-        .directive('atlasDropdownMenu', atlasDropdownMenu);
-
-    function atlasDropdownMenu() {
-        var directive = {
-            restrict: 'E',
-            scope: {
-                template: '=template',
+        .component('atlasDropdownMenu', {
+            bindings: {
+                label: '@',
             },
-            replace: true,
-            templateUrl: function(element, attrs) {
-                return 'modules/header/components/menu/templates/'+attrs.template+'.html';
-            },
-            link: linkFunction
-        };
-        return directive;
-    }
+            transclude: true,
+            templateUrl:'modules/header/components/menu/dropdown-menu.html',
+            controller: AtlasDropdownMenuController,
+            controllerAs: 'vm',
+        });
 
-    function linkFunction(scope) {
-        scope.visible = true;
+    AtlasDropdownMenuController.$inject = [];
 
-        console.log(scope);
+    function AtlasDropdownMenuController() {
+        var vm = this;
+            vm.isVisible = false;
+
+
     }
 
 })();
-
