705128473758250151dcc62de4816ab6133a2d9c
Erik Nijland <e.n.nijland@gmail.com>
2016-07-27 15:33:17 +0200

    user service refactor

--- modules/shared/services/api/api.factory.js
+++ modules/shared/services/api/api.factory.js
@@ -5,17 +5,27 @@
         .module('dpShared')
         .factory('api', apiFactory);
 
-    apiFactory.$inject = ['$http'];
+    apiFactory.$inject = ['$http', 'user'];
 
-    function apiFactory ($http) {
+    function apiFactory ($http, user) {
         return {
             getByUrl: getByUrl
         };
 
         function getByUrl (url) {
+            var headers = {},
+                userState;
+
+            userState = user.getStatus();
+
+            if (userState.loggedIn) {
+                headers.Authorization = 'Bearer ' + userState.accessToken;
+            }
+
             return $http({
                 method: 'GET',
                 url: url,
+                headers: headers,
                 /*
                 Caching is set to false to enforce distinction between logged in users and guests. The API doesn't
                 support tokens yet.
