7a71c439b737c3a93f80960f83bac0386a2292c4
Erik Nijland <e.n.nijland@gmail.com>
2016-07-28 11:17:22 +0200

    Test cases for loggin in with an authorization header.

--- modules/shared/services/api/api.factory.test.js
+++ modules/shared/services/api/api.factory.test.js
@@ -1,14 +1,34 @@
 describe('The api factory', function () {
-    var $httpBackend,
+    var $http,
+        $httpBackend,
         api,
-        mockedApiData;
+        mockedApiData,
+        isLoggedIn;
 
     beforeEach(function () {
         angular.mock.module(
-            'dpShared'
+            'dpShared',
+            {
+                user: {
+                    getStatus: function () {
+                        if (isLoggedIn) {
+                            return {
+                                accessToken: 'MY_FAKE_ACCESS_TOKEN',
+                                isLoggedIn: true
+                            };
+                        } else {
+                            return {
+                                accessToken: null,
+                                isLoggedIn: false
+                            };
+                        }
+                    }
+                }
+            }
         );
 
-        angular.mock.inject(function (_$httpBackend_, _api_) {
+        angular.mock.inject(function (_$http_, _$httpBackend_, _api_) {
+            $http = _$http_;
             $httpBackend = _$httpBackend_;
             api = _api_;
         });
@@ -19,6 +39,13 @@ describe('The api factory', function () {
         };
 
         $httpBackend.whenGET('https://api-acc.datapunt.amsterdam.nl/bag/verblijfsobject/123/').respond(mockedApiData);
+
+        isLoggedIn = false;
+    });
+
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
     });
 
     it('returns the data as a promise', function () {
@@ -32,5 +59,27 @@ describe('The api factory', function () {
 
         //This checks that it doesn't return the $http response which contains meta information as well.
         expect(returnValue).toEqual(mockedApiData);
+    });
+
+    it('adds an Authorization header if the user is logged in', function () {
+        //Not logged in
+        isLoggedIn = false;
+
+        $httpBackend.expectGET(
+            'https://api-acc.datapunt.amsterdam.nl/bag/verblijfsobject/123/',
+            $http.defaults.headers.common
+        );
+        api.getByUrl('https://api-acc.datapunt.amsterdam.nl/bag/verblijfsobject/123/');
+        $httpBackend.flush();
+
+        //Logged in
+        isLoggedIn = true;
+
+        $httpBackend.expectGET(
+            'https://api-acc.datapunt.amsterdam.nl/bag/verblijfsobject/123/',
+            angular.merge({}, $http.defaults.headers.common, {Authorization: 'Bearer MY_FAKE_ACCESS_TOKEN'})
+        );
+        api.getByUrl('https://api-acc.datapunt.amsterdam.nl/bag/verblijfsobject/123/');
+        $httpBackend.flush();
     });
 });
