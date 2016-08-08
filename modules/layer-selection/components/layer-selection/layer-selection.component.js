afdda178f2df5dd92795bb55cb8734554ce7dd56
Kfir Breger <kfir@cubicalwave.com>
2016-08-08 13:12:39 +0200

    Removing debug logging

--- modules/layer-selection/components/layer-selection/layer-selection.component.js
+++ modules/layer-selection/components/layer-selection/layer-selection.component.js
@@ -18,7 +18,7 @@
 
     function AtlasLayerSelectionController (BASE_LAYERS, OVERLAYS, store, ACTIONS) {
         var vm = this;
-        console.log(vm.activeOverlays);
+
         vm.allBaseLayers = BASE_LAYERS;
 
         vm.setBaseLayer = function (baseLayer) {
@@ -37,7 +37,6 @@
                     label: OVERLAYS.SOURCES[overlaySlug].label
                 };
             });
-            console.log(formattedOverlays);
             return formattedOverlays;
         });
 
