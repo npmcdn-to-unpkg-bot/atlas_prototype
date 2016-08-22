4615e4e3a5abbf5fad2a370e3ccd54650bd34274
Kfir Breger <kfir@cubicalwave.com>
2016-08-22 15:09:36 +0200

    Showing and hiding layers count as per the specs

--- modules/map/components/statusbar/statusbar.component.js
+++ modules/map/components/statusbar/statusbar.component.js
@@ -20,13 +20,26 @@
         vm.visible = false;
         // Layers counts
         vm.activeLayers = vm.overlays.length;
+        vm.visibleLayers = 0;
+        for (var i = 0;i < vm.overlays.length;i++) {
+            if (vm.overlays[i].visibility) {
+                vm.visibleLayers++;
+            }
+        }
         // Watching from component scope where the controller is called vm
         // as per controllerAs
         $scope.$watch('vm.overlays', function(newVal) {
+            // Updating active and visible layers count
             if (newVal) {
-                vm.activeLayers = newVal.length;    
+                vm.activeLayers = newVal.length;
+                vm.visibleLayers = 0;
+                for (var i = 0;i < newVal.length;i++) {
+                    if (newVal[i].visibility) {
+                        vm.visibleLayers++;
+                    }
+                }
             }
-        });
+        }, true);
         
         vm.switchLegendPane = function () {
             // Flipping the button
