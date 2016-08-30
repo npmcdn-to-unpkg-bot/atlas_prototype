8340439a796f15e6320b88d4f947e48f3a41d891
Erik Nijland <e.n.nijland@gmail.com>
2016-08-30 16:27:27 +0200

    New unit test work in progress

--- modules/shared/components/straatbeeld-thumbnail/straatbeeld-thumbnail.component.test.js
+++ modules/shared/components/straatbeeld-thumbnail/straatbeeld-thumbnail.component.test.js
@@ -1,6 +1,9 @@
 describe('The dp-straatbeeld-thumbnail component', function () {
     var $compile,
         $rootScope,
+        $q,
+        hasNearbyPanorama,
+        api,
         store,
         ACTIONS;
 
@@ -11,19 +14,40 @@ describe('The dp-straatbeeld-thumbnail component', function () {
                 sharedConfig: {
                     STRAATBEELD_THUMB_URL: 'http://fake.straatbeeld.url/path/'
                 },
+                api: {
+                    getByUrl: function () {
+                        var q = $q.defer(),
+                            response;
+
+                        if (hasNearbyPanorama) {
+                            response = {
+                                url: 'http://www.example.com/panorama-plaatjes/abf123/'
+                            };
+                        } else {
+                            response = [];
+                        }
+
+                        q.resolve(response);
+
+                        return q.promise;
+                    }
+                },
                 store: {
                     dispatch: function () {}
                 }
             }
         );
 
-        angular.mock.inject(function (_$compile_, _$rootScope_, _store_, _ACTIONS_) {
+        angular.mock.inject(function (_$compile_, _$rootScope_, _$q_, _api_, _store_, _ACTIONS_) {
             $compile = _$compile_;
             $rootScope = _$rootScope_;
+            $q = _$q_;
+            api = _api_;
             store = _store_;
             ACTIONS = _ACTIONS_;
         });
 
+        spyOn(api, 'getByUrl').and.callThrough();
         spyOn(store, 'dispatch');
     });
 
@@ -44,25 +68,39 @@ describe('The dp-straatbeeld-thumbnail component', function () {
         return component;
     }
 
-    it('loads a thumbnail based on a location', function () {
-        var component = getComponent([52.369, 4.963]);
+    it('uses a loading indicator', function () {
 
-        expect(component.find('img').attr('src'))
-            .toBe('http://fake.straatbeeld.url/path/?lat=52.369&lon=4.963&width=240');
     });
 
-    it('wraps the thumbnail inside a button that triggers FETCH_STRAATBEELD', function () {
-        var component = getComponent([52.369, 4.963]);
+    describe('when a panorama has been found', function () {
+        it('shows a thumbnail based on a location', function () {
+            var component = getComponent([52.369, 4.963]);
 
-        expect(component.find('button img')).toBeDefined();
-        expect(component.find('button').length).toBe(1);
-        expect(component.find('img').length).toBe(1);
+            //$rootScope.$apply();
 
-        expect(component.find('button').click());
+            expect(component.find('img').attr('src'))
+                .toBe('http://www.example.com/panorama-plaatjes/abf123/');
+        });
 
-        expect(store.dispatch).toHaveBeenCalledWith({
-            type: ACTIONS.FETCH_STRAATBEELD,
-            payload: [52.369, 4.963]
+        it('wraps the thumbnail inside a button that triggers FETCH_STRAATBEELD', function () {
+            var component = getComponent([52.369, 4.963]);
+
+            expect(component.find('button img')).toBeDefined();
+            expect(component.find('button').length).toBe(1);
+            expect(component.find('img').length).toBe(1);
+
+            expect(component.find('button').click());
+
+            expect(store.dispatch).toHaveBeenCalledWith({
+                type: ACTIONS.FETCH_STRAATBEELD,
+                payload: [52.369, 4.963]
+            });
+        });
+    });
+
+    describe('when there is no nearby panorama', function () {
+        it('shows a message that there is nothing to show here', function () {
+
         });
     });
 });
