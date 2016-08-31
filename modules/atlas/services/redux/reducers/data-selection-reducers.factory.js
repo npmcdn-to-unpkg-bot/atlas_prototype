f9838d7b84f082b383dbec111ff18d6fdecf3e88
Erik Nijland <e.n.nijland@gmail.com>
2016-08-31 13:37:05 +0200

    Triggering SHOW_DATA_SELECTION now updates the rest of the state.

--- modules/atlas/services/redux/reducers/data-selection-reducers.factory.js
+++ modules/atlas/services/redux/reducers/data-selection-reducers.factory.js
@@ -5,9 +5,9 @@
         .module('atlas')
         .factory('dataSelectionReducers', dataSelectionReducersFactory);
 
-    dataSelectionReducersFactory.$inject = ['ACTIONS'];
+    dataSelectionReducersFactory.$inject = ['ACTIONS', 'DEFAULT_STATE'];
 
-    function dataSelectionReducersFactory (ACTIONS) {
+    function dataSelectionReducersFactory (ACTIONS, DEFAULT_STATE) {
         var reducers = {};
 
         reducers[ACTIONS.SHOW_DATA_SELECTION] = showDataSelectionReducer;
@@ -24,8 +24,29 @@
         function showDataSelectionReducer (oldState, payload) {
             var newState = angular.copy(oldState);
 
-            newState.dataSelection = payload;
-            newState.dataSelection.page = 1;
+            newState.map = {
+                baseLayer: oldState.map.baseLayer,
+                overlays: oldState.map.overlays,
+                viewCenter: DEFAULT_STATE.map.viewCenter,
+                zoom: DEFAULT_STATE.map.zoom,
+                highlight: null,
+                showLayerSelection: false,
+                isFullscreen: false,
+                isLoading: false
+            };
+
+            newState.search = null;
+            newState.page = null;
+            newState.detail = null;
+            newState.straatbeeld = null;
+
+            newState.dataSelection = angular.merge(
+                {},
+                payload,
+                {
+                    page: 1
+                }
+            );
 
             return newState;
         }
