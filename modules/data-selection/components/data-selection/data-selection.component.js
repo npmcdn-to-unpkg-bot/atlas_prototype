a8e121b79422303e7754b2e82ab37a92c222aa13
Erik Nijland <e.n.nijland@gmail.com>
2016-08-31 15:17:26 +0200

    The map is no longer always visible. The mock dp-data-selection component is in place and aware of the relevant part of the application state. Encapsulation inside the atlas module is done except for the tests.

--- modules/data-selection/components/data-selection/data-selection.component.js
+++ modules/data-selection/components/data-selection/data-selection.component.js
@@ -5,6 +5,9 @@
         .module('dpDataSelection')
         .component('dpDataSelection', {
             templateUrl: 'modules/data-selection/components/data-selection/data-selection.html',
+            bindings: {
+                state: '='
+            },
             controller: DpDataSelectionController,
             controllerAs: 'vm'
         });
