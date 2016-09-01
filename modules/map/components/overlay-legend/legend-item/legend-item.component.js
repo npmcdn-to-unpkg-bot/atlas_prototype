887488a5165524aed5a9511d06c5111b2eca5a96
Kfir Breger <kfir@cubicalwave.com>
2016-09-01 11:56:33 +0200

    Adding alt text to the different icons

--- modules/map/components/overlay-legend/legend-item/legend-item.component.js
+++ modules/map/components/overlay-legend/legend-item/legend-item.component.js
@@ -21,6 +21,7 @@
 		// Setting legend and label
 		vm.label = OVERLAYS.SOURCES[vm.overlay.id].label_short;
 		vm.legend = OVERLAYS.SOURCES[vm.overlay.id].legend;
+		vm.icon_alt_text = 'Status Icon';
 		// Checking for external link
 		if (vm.legend && !OVERLAYS.SOURCES[vm.overlay.id].external) {
 			vm.legend = mapConfig.OVERLAY_ROOT + vm.legend;
@@ -33,11 +34,14 @@
 				if (vm.zoom >= OVERLAYS.SOURCES[vm.overlay.id].minZoom &&
 	            	vm.zoom <= OVERLAYS.SOURCES[vm.overlay.id].maxZoom) {
 					icon += 'visible.svg';
+					vm.icon_alt_text = 'Overlay zichtbaar';
 				} else {
 					icon += 'invisible-zoomlevel.svg';
+					vm.icon_alt_text = 'Overlay onzichtbaar door zoom level';
 				}
 			} else {
 				icon += 'invisible-hidden.svg';
+				vm.icon_alt_text = 'Overlay verborgen';
 			}
 			return icon;	
 		};
