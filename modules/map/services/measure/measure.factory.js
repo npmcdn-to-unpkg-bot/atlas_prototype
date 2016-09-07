478adc6cddce5001819006a2628aba7033da7990
Erik Nijland <e.n.nijland@gmail.com>
2016-09-07 14:00:15 +0200

    Hide the active overlays on 'measurestart'.

--- modules/map/services/measure/measure.factory.js
+++ modules/map/services/measure/measure.factory.js
@@ -5,9 +5,9 @@
         .module('dpMap')
         .factory('measure', measureFactory);
 
-    measureFactory.$inject = ['$document', 'L', 'MEASURE_CONFIG'];
+    measureFactory.$inject = ['$document', '$rootScope', 'L', 'MEASURE_CONFIG', 'store', 'ACTIONS'];
 
-    function measureFactory ($document, L, MEASURE_CONFIG) {
+    function measureFactory ($document, $rootScope, L, MEASURE_CONFIG, store, ACTIONS) {
         return {
             initialize: initialize
         };
@@ -19,11 +19,16 @@
             measureControl.addTo(leafletMap);
 
             leafletMeasureHtml = measureControl.getContainer();
-
-            //Add a class to leaflet-measure control
             leafletMeasureHtml.className += ' s-leaflet-measure';
-
             $document[0].querySelector('.js-leaflet-measure').appendChild(leafletMeasureHtml);
+
+            leafletMap.on('measurestart', function () {
+                $rootScope.$applyAsync(function () {
+                    store.dispatch({
+                        type: ACTIONS.HIDE_ACTIVE_OVERLAYS
+                    });
+                });
+            });
         }
     }
 })();
