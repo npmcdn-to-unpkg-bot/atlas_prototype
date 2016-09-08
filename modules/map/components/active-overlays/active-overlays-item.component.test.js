1a2b4c8688f8b8b8ddb7fc3e71baef4b0af8b64c
Erik Nijland <e.n.nijland@gmail.com>
2016-09-08 15:24:49 +0200

    Unit tests for dp-active-overlays-item

--- modules/map/components/active-overlays/active-overlays-item.component.test.js
+++ modules/map/components/active-overlays/active-overlays-item.component.test.js
@@ -14,21 +14,28 @@ describe('The dp-active-overlays-item component', function () {
                 $provide.constant('OVERLAYS', {
                     SOURCES: {
                         overlay_without_legend: {
+                            label_short: 'Overlay A',
                             minZoom: 8,
                             maxZoom: 10
                         },
                         overlay_with_internal_legend: {
+                            label_short: 'Overlay B',
                             minZoom: 10,
                             maxZoom: 14,
                             legend: 'blabla.png'
                         },
                         overlay_with_external_legend: {
-                            minZoom: 12,
+                            label_short: 'Overlay C',
+                            minZoom: 10,
                             maxZoom: 16,
                             legend: 'http://not-atlas.example.com/blabla.png',
                             external: true
                         }
                     }
+                });
+
+                $provide.factory('dpLinkDirective', function () {
+                    return {};
                 });
             }
         );
@@ -39,39 +46,113 @@ describe('The dp-active-overlays-item component', function () {
         });
     });
 
-    function getComponent () {
+    function getComponent (overlay, isVisible, zoom) {
+        var component,
+            element,
+            scope;
 
+        element = document.createElement('dp-active-overlays-item');
+        element.setAttribute('overlay', overlay);
+        element.setAttribute('is-visible', 'isVisible');
+        element.setAttribute('zoom', 'zoom');
+
+        scope = $rootScope.$new();
+        scope.isVisible = isVisible;
+        scope.zoom = zoom;
+
+        component = $compile(element)(scope);
+        scope.$apply();
+
+        return component;
     }
 
     it('shows uses the short label from the OVERLAYS config', function () {
+        var component = getComponent('overlay_without_legend', true, 8);
 
+        expect(component.find('h3').eq(0).text()).toContain('Overlay A');
     });
 
     it('has an optional legend image', function () {
+        var component;
+
         //No legend
+        component = getComponent('overlay_without_legend', true, 10);
+        expect(component.find('img').length).toBe(0);
 
         //A self-hosted legend
+        component = getComponent('overlay_with_internal_legend', true, 10);
+        expect(component.find('img').length).toBe(1);
 
         //An externally hosted legend
+        component = getComponent('overlay_with_external_legend', true, 10);
+        expect(component.find('img').length).toBe(1);
     });
 
     it('shows a message for manually hidden overlays', function () {
+        var component;
 
+        //When visible
+        component = getComponent('overlay_without_legend', true, 10);
+        expect(component.text()).not.toContain('Handmatig onzichtbaar gemaakt.');
+
+        //When invisible
+        component = getComponent('overlay_without_legend', false, 10);
+        expect(component.text()).toContain('Handmatig onzichtbaar gemaakt.');
     });
 
     it('shows a message for hidden overlays caused by the zoom level', function () {
+        var component,
+            i;
 
+        //When visible
+        for (i = 8; i <= 10; i++) {
+            component = getComponent('overlay_without_legend', true, i);
+            expect(component.text()).not.toContain('Niet zichtbaar op dit zoomniveau.');
+        }
+
+        //When invisible
+        for (i = 11; i <= 16; i++) {
+            component = getComponent('overlay_without_legend', true, i);
+            expect(component.text()).toContain('Niet zichtbaar op dit zoomniveau.');
+        }
     });
 
     it('won\'t show the hidden because of the zoom level message when the layer has been hidden manually', function () {
+        var component,
+            i;
 
+        //When invisible
+        for (i = 11; i <= 16; i++) {
+            component = getComponent('overlay_without_legend', false, i);
+            expect(component.text()).not.toContain('Niet zichtbaar op dit zoomniveau.');
+        }
     });
 
     it('has a button to hide the overlay, even if it\'s already hidden because of the zoom level', function () {
+        var component;
 
+        //With a supported zoom level
+        component = getComponent('overlay_without_legend', true, 10);
+        expect(component.find('dp-link').length).toBe(1);
+        expect(component.find('dp-link').text()).toContain('Kaartlaag onzichtbaar maken');
+
+        //With an unsupported zoom level
+        component = getComponent('overlay_without_legend', true, 9);
+        expect(component.find('dp-link').length).toBe(1);
+        expect(component.find('dp-link').text()).toContain('Kaartlaag onzichtbaar maken');
     });
 
     it('has a button to show the overlay, even if it can\'t be shown on the current zoom level', function () {
+        var component;
 
+        //With a supported zoom level
+        component = getComponent('overlay_without_legend', true, 10);
+        expect(component.find('dp-link').length).toBe(1);
+        expect(component.find('dp-link').text()).toContain('Kaartlaag onzichtbaar maken');
+
+        //With an unsupported zoom level
+        component = getComponent('overlay_without_legend', true, 9);
+        expect(component.find('dp-link').length).toBe(1);
+        expect(component.find('dp-link').text()).toContain('Kaartlaag onzichtbaar maken');
     });
 });
